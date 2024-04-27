366c0511d5d34ed1efd19ae2c353fb2356627dd0
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-04-27 13:03:21 +0000

    mysql advanced

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---we are all unique
---creating a table with constraints
+-- we are all unique
+-- creating a table with constraints
 CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
