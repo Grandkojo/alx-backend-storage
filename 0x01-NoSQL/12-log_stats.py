e38f4539e640ae0753ac20e5f8c38c02776942cb
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-01 10:04:48 -0700

    no sql

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+from pymongo import MongoClient
+
+if "__name__" == "__main__":
+    
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginxstuff = client.logs.nginx
+    methods = ['GET', 'POSt', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    print(f'{nginxstuff.count_documents({})} logs')
+
+    print("Methods:")
+    for method in methods:
+        number = nginxstuff.count_documents({"methods": method})
+        print(f'\t{method}: {number}')
+
+    number = nginxstuff.count_documents({"method": "GET", "path": "/status"})
+    print(f'{number} status checks')
+
