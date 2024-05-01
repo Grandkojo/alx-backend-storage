7e68a03abb27062a1490ed0b623521ea6cf18591
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-05-01 14:02:51 -0700

    no  sql

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,17 +3,17 @@
 
 from pymongo import MongoClient
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginxstuff = client.logs.nginx
-    methods = ['GET', 'POSt', 'POST', 'PUT', 'PATCH', 'DELETE']
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
     print(f'{nginxstuff.count_documents({})} logs')
 
     print("Methods:")
     for method in methods:
-        number = nginxstuff.count_documents({"methods": method})
+        number = nginxstuff.count_documents({"method": method})
         print(f'\t{method}: {number}')
 
     number = nginxstuff.count_documents({"method": "GET", "path": "/status"})
